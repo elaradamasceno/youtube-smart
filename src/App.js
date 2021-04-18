7c501699613d325ad81a3b598d38f1d4ce9293fb
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-04-18 19:13:58 -0300

    ExperienceBar - in progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import './styles/global.css';
+import { ExperienceBar } from './components/ExperienceBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { AreaUser } from './pages/AreaUser';
+import { Search } from './pages/Search';
+import { Favorites } from './pages/Favorites';
+import { Page404 } from './pages/Page404';
+
 
 function App() {
   return (
-    <div className="App">
-      Olá
+    <div className="container">
+      <Router>
+        <ExperienceBar/>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/area-user">
+            <AreaUser />
+          </Route>
+
+          <Route path="/search">
+            <Search />
+          </Route>
+
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+
+          <Route>
+            <Page404/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
