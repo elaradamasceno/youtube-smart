d535299d10867d2b7dba41bbcdb2c5f042568894
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-04-21 15:51:19 -0300

    Search in progress

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -20,31 +20,35 @@ export function Search(){
 
     return(
         <div className="search">
-            <h2>Pesquisar</h2>
-            <div className="content-search">
-                <div className="field-search">
-                    <Input 
-                        id="field-search" 
-                        className="navigation" 
-                        size="large" 
-                        placeholder="Buscar" 
-                        defaultValue={value}
-                        value={value}
-                    />
-
-                    <div className="buttons-search">
-                        <Button 
-                            id="btn-search" 
+            <div>
+                <h2>Pesquisar</h2>
+                <div className="content-search">
+                    <div className="field-search">
+                        <Input 
+                            id="field-search" 
                             className="navigation" 
-                            type="primary" 
-                            htmlType="submit"
-                        >
-                            Entrar
-                        </Button>
+                            size="large" 
+                            placeholder="Buscar" 
+                            defaultValue={value}
+                            value={value}
+                        />
+
+                        <div className="buttons-search">
+                            <Button 
+                                id="btn-search" 
+                                className="navigation" 
+                                type="primary" 
+                                disabled={value !== '' ? false : true}
+                            >
+                                Entrar
+                            </Button>
+                        </div>
                     </div>
+                    <Keyboard getLetters={getLetters} clearLetter={clearLetter} typeScreen="login" />
                 </div>
-                <Keyboard getLetters={getLetters} clearLetter={clearLetter} typeScreen="login" />
             </div>
+
+
         </div>
     )
 }
