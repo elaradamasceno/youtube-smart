1efe5cf75471786e684246f28f4e09e0736b4ce8
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-04-21 15:58:48 -0300

    Search result

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,7 +5,8 @@ import { Keyboard } from  '../components/Keyboard';
 import '../styles/components/Search.css';
 
 export function Search(){
-    const [ value, setValue ] = useState('');
+    const [ value, setValue ] = useState('UIAAA');
+    const [ searchIsVisible, setSearchIsVisible ] = useState(false);
 
     function getLetters(data){
         let element = document.getElementById('field-search');
@@ -15,40 +16,52 @@ export function Search(){
 
     function clearLetter(data){ 
         let element = document.getElementById('field-search');
-        element.value = value.slice(0, -data);
+        setValue(value.slice(0, -data)) 
+    }
+
+    function actionButtonSearch(){
+        console.log('opa')
     }
 
     return(
         <div className="search">
-            <div>
-                <h2>Pesquisar</h2>
-                <div className="content-search">
-                    <div className="field-search">
-                        <Input 
-                            id="field-search" 
-                            className="navigation" 
-                            size="large" 
-                            placeholder="Buscar" 
-                            defaultValue={value}
-                            value={value}
-                        />
-
-                        <div className="buttons-search">
-                            <Button 
-                                id="btn-search" 
+            { searchIsVisible ? (
+                <div>
+                    <h2>Pesquisar</h2>
+                    <div className="content-search">
+                        <div className="field-search">
+                            <Input 
+                                id="field-search" 
                                 className="navigation" 
-                                type="primary" 
-                                disabled={value !== '' ? false : true}
-                            >
-                                Entrar
-                            </Button>
+                                size="large" 
+                                placeholder="Buscar" 
+                                defaultValue={value}
+                                value={value}
+                            />
+
+                            <div className="buttons-search">
+                                <Button 
+                                    id="btn-search" 
+                                    className="navigation" 
+                                    type="primary" 
+                                    disabled={value !== '' ? false : true}
+                                    onClick={actionButtonSearch}
+                                >
+                                    Buscar
+                                </Button>
+                            </div>
                         </div>
+                        <Keyboard getLetters={getLetters} clearLetter={clearLetter} typeScreen="login" />
                     </div>
-                    <Keyboard getLetters={getLetters} clearLetter={clearLetter} typeScreen="login" />
                 </div>
-            </div>
+            ) : (
+                <div>
+                    <h2>{value}</h2>
+                    <div className="content-result-search">
 
-
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
