83b0da5c3b7d0ceebeba2da83e058bda3b0c135b
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-04-22 12:10:57 -0300

    Favorites - layout user not logged

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,29 @@
+import { Button } from 'antd';
+import '../styles/components/Favorites.css';
+
 export function Favorites(){
     return(
-        <div>
-            Favorites
+        <div className="favorites">
+            <div className="user-logged">
+
+            </div>
+
+            <div className="user-not-logged">
+                <div class="content-not-logged">
+                    <div>
+                        <div className="card-video"></div>
+                        <div className="card-video"></div>
+                        <div className="card-video"></div>
+                    </div>
+                    <div className="info-not-logged">
+                        <h2>Assista vídeos que você salvou</h2>
+                        <span>Faça login para ver seus favoritos</span>
+                    </div>
+                    <div className="button-login">
+                        <Button id="redirect-login" type="primary" size="large">FAZER LOGIN</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
