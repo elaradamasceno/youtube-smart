cee92c85cd60e580440da65f5e9a9fcd193ae1a7
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-04-23 11:40:06 -0300

    Ajuste exibição de videos home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { ArrowDownOutlined } from '@ant-design/icons';
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
 import { ListVideos } from '../components/ListVideos';
 import '../styles/components/Home.css';
 
 export function Home(){
-    let teste = [];
-
     const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
     const YOUTUBE_API_KEY =  process.env.REACT_APP_YOUTUBE_API_KEY;
 
     const [ errorRequest, setErrorRequest ] = useState(false);
     const [ resultAPI, setResultAPI ] = useState([]);
     const [ items, setItems ] = useState([]);
-    const [ trilhoMusic, setTrilhoMusic ] = useState([]);
+    const [ visibleAllVideos, setVisibleAllVideos ] = useState(false);
 
     const resultRequest = [];
 
@@ -37,7 +35,8 @@ export function Home(){
         if(music === null && news === null && sports === null){
             channels.forEach((element, index) => {
                 let url = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${element.channel}&part=snippet,id&order=date&maxResults=20`;
-    
+                let allItems = [];
+
                 axios.get(url)
                 .then(res => {
                     if(res.status === 200){
@@ -48,32 +47,51 @@ export function Home(){
                             data: res.data
                         });
 
+                        allItems.push({
+                            type: element.type,
+                            data: res.data.items
+                        });
+
                         setResultAPI(resultRequest);
+                        setItems(allItems);
                     }
                 })
             });
         }
         else{
             let arrayStorage = [JSON.parse(music), JSON.parse(news), JSON.parse(sports)];
+            let allItems = [];
 
             channels.forEach((element, index) => {
                 resultRequest.push({
                     title: element.title,
                     type: element.type,
                     data: arrayStorage[index]
-                })
+                });
+
+                allItems.push({
+                    type: element.type,
+                    data: arrayStorage[index].items.slice(0, 4)
+                });
+
             });
-
-            setResultAPI(resultRequest);
-            // console.log(resultRequest)
-
-   
+            
+            setItems(allItems);
+            setResultAPI(resultRequest);  
         }
     }
 
-    function actionMoreItems(i){
-        console.log(i)
-        // setResultAPI(trilhoMusic);
+    function actionMoreItems(index, element){
+        setVisibleAllVideos(!visibleAllVideos);
+        let elementType = [element.type];
+        let result = []
+
+        if(visibleAllVideos)
+            result = items.reduce((acc, o) => acc.concat(elementType.includes(o.type) ? Object.assign(o, { data: element.data.items}) : o), []);
+        else
+            result = items.reduce((acc, o) => acc.concat(elementType.includes(o.type) ? Object.assign(o, { data: element.data.items.slice(0, 4)}) : o), []);
+
+        setItems(result);
     }
 
     useEffect(() => {
@@ -84,14 +102,13 @@ export function Home(){
     return(
         <div className="home">
             {resultAPI.map((element, index) => {
-                console.log(element)
                 return(
                     <div key={index}>
                         <h2>{element.title}</h2>
                         <div className="content-videos">
-                            <ListVideos listVideos={element.data.items.slice(0, 4)} typeScreen="home" />
-                            <a className="btn-music" onClick={() => actionMoreItems(index)}>
-                                <ArrowDownOutlined />
+                            <ListVideos listVideos={items[index].data} typeScreen="home" />
+                            <a className="btn-music" onClick={() => actionMoreItems(index, element)}>
+                                { visibleAllVideos ? ( <ArrowDownOutlined /> ) : ( <ArrowUpOutlined /> )}
                             </a>
                         </div>
                     </div>
