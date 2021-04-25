b313bc65e2ce9759d0daab9c74952024fefd5e78
Elara Damasceno <elaradamasceno.edl@gmail.com>
2021-04-25 14:22:10 -0300

    Alteração layout login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,74 +5,86 @@ import { Steps, Form, Input, Button} from 'antd';
 import { Keyboard } from  '../components/Keyboard';
 import '../styles/components/Login.css';
 
-let fieldId;
-
+let current = 0;
 export function Login({ verifyIsLogged }){
     const history = useHistory();
-    const [ visibleKeyboard, setVisibleKeyboard ] = useState(false);
     const [valueMail, setMail] = useState('');
     const [valueName, setName] = useState('');
     const [valuePassword, setPassword] = useState('');
+    const [fieldRequired, setFieldRequired] = useState(false);
 
-    const [currentStep, setCurrentStep] = useState(0);
+    const [currentStep, setCurrentStep] = useState(current);
     const { Step } = Steps;
 
 
-    function fieldFocus(id){
-        setVisibleKeyboard(true);
-        fieldId = id;
-    }
-
     function getLetters(data){
         const fieldsId = ['name', 'mail', 'password'];
-        let element = document.getElementById(`${fieldsId[currentStep]}`);
+        let element = document.getElementById(`${fieldsId[current]}`);
 
-        if(fieldsId[currentStep] === 'name'){
+        if(fieldsId[current] === 'name'){
             let value = element.value + data;
             setName(value);
         }
 
-        if(fieldsId[currentStep] === 'mail'){
+        if(fieldsId[current] === 'mail'){
             let value = element.value + data;
             setMail(value);
         }
 
-        if(fieldsId[currentStep] === 'password'){
+        if(fieldsId[current] === 'password'){
             let value = element.value + data;
             setPassword(value);
         }
     }
 
     function clearLetter(data){ 
-        if(fieldId === 'name'){
+        const fieldsId = ['name', 'mail', 'password'];
+        if(fieldsId[currentStep] === 'name'){
             let value = valueName.slice(0, -data);
             setName(value);
         }
 
-        if(fieldId === 'mail'){
+        if(fieldsId[currentStep] === 'mail'){
             let value = valueMail.slice(0, -data);
             setMail(value);
         }
 
-        if(fieldId === 'password'){
+        if(fieldsId[currentStep] === 'password'){
             let value = valuePassword.slice(0, -data);
             setPassword(value);
         }
     }
 
-    function onSubmit(){
-        window.localStorage.setItem('userName', valueName);
-        history.push("/");
-
-        verifyIsLogged(true);
-    }
-
     function changeStep(){
-
+        if(currentStep === 0){
+            if(valueName === "")
+                setFieldRequired(true);
+            else{
+                setFieldRequired(false);
+                current = current+1;
+                setCurrentStep(current);
+                window.localStorage.setItem('userName', valueName);
+            }
+        }
+        else if(currentStep === 1){
+            if(valueMail === "")
+                setFieldRequired(true)
+            else{
+                setFieldRequired(false);
+                current = current+1;
+                setCurrentStep(current);
+            }
+        }
+        else if(currentStep === 2){
+            if(valuePassword === "")
+                setFieldRequired(true);
+            else{
+                setFieldRequired(false);
+                history.push("/");
+                verifyIsLogged(true);
+            }
+        }
     }
-
-    useEffect((e) => {}, [fieldId]);
-    // useEffect(() => { document.getElementById('basic_name').focus(); }, []);
 
 
     return(
@@ -81,7 +93,7 @@ export function Login({ verifyIsLogged }){
 
             <div className="content-login">
                 <div>
-                    <Steps current={currentStep} onChange={changeStep}>
+                    <Steps current={currentStep}>
                         <Step />
                         <Step />
                         <Step />
@@ -120,12 +132,14 @@ export function Login({ verifyIsLogged }){
                             value={valuePassword}
                         />
                     }
+                    { fieldRequired && <span className="field-required">Este campo é obrigatório</span> }
 
                     <div className="button-login">
                         <Button 
                             id="btn-login-submit" 
                             className="navigation" 
                             type="primary" 
+                            onClick={changeStep}
                         >
                             {currentStep === 0 || currentStep === 1 ? 'Próximo' : 'Entrar'}
                         </Button>
